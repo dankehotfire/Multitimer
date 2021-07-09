bd9ca170075068110eee76a43b83580e0a8edcbd
Danil Nurgaliev <Danilkchik@gmail.com>
2021-07-09 13:29:43 +0300

    Setup project

---
+++ Multitimer/Multitimer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Multitimer
+//
+//  Created by Danil Nurgaliev on 09.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+}
+
